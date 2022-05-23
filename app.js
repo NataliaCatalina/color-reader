f80f81920594eddd9f03ed8c979ea3bc4ca483c3
Natalia.Catalina <ntlc.ramirez@gmail.com>
2022-05-23 17:16:33 +1200

    asd

---
+++ app.js
@@ -0,0 +1,4 @@
+const colorInput = document.querySelector('#colorInput')
+const btnDisplay = document.querySelector('#btnDisplay')
+const colorExa = document.querySelector('#colorExa')
+const cardColor = document.querySelector('#cardColor')
