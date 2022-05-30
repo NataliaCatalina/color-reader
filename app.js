4c052045aa7ebd987366934452f939d032814905
Natalia.Catalina <ntlc.ramirez@gmail.com>
2022-05-30 14:08:38 +1200

    funtional color reader

--- app.js
+++ app.js
@@ -2,3 +2,12 @@ const colorInput = document.querySelector('#colorInput')
 const btnDisplay = document.querySelector('#btnDisplay')
 const colorExa = document.querySelector('#colorExa')
 const cardColor = document.querySelector('#cardColor')
+
+btnDisplay.addEventListener('click', () => {
+    colorExa.textContent = colorInput.value
+    cardColor.style.backgroundColor = colorInput.value
+    navigator.clipboard
+        .writeText(colorInput.value)
+        .then(() => console.log('copied text'))
+        .catch((e) => console.log(e))
+})
